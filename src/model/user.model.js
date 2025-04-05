9b336b39c86aa758c1a245cda37ce6b593c3cd01
k2006 <k2006soni@gmail.com>
2025-04-05 23:11:44 +0530

    fix: correct dotenv path and improve user model schema

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,80 +1,93 @@
-import mongoose,{Schema} from "mongoose"
-import {jwt} from 'jsonwebtoken'
-const UserSchema=new Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true
-    },
-    fullname:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true
-    },
-    avatar:{
-        type:String,  //cloudanary url
-        required:true,
-    },
-    coverImage:{
-        type:String,
-    },
-    watchHistory:[
-        {
-            type:Schema.Types.ObjectId,
-            ref:"Video"
-        }
-    ],
-    password:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true
-    },
-    refreshTokens:{
-        type:String
-    }
-})
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
-UserSchema.pre("save",async function(next){
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
-    this.password=await bcrypt.hash(this.password,10)
+
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
-UserSchema.methods.isPasswordCorrect=async function(password){
-    return await bcrypt.compare(password,this.password)
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
 }
-UserSchema.methods.generateAccessTokens=function(){
-    jwt.sign(
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
         {
-            _id:this.id,
-            email:this.email
-        },process.env.ACCESS_TOKENS_SECTET,{
-            expiresIn:ACCESS_TOKENS_EXPIRY
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
     )
 }
-UserSchema.methods.generateRefreshTokens=function(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
-            _id:this.id,
-            email:this.email
-        },process.env.REFRESH_TOKEN_SECTET,{
-            expiresIn:REFRESH_TOKEN_EXPIRY
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
     )
 }
 
-export const User=mongoose.model("User",UserSchema) 
+export const User = mongoose.model("User", userSchema)
