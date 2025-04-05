957addc701b5acf80c0385a75c53077d9264dfad
k2006 <k2006soni@gmail.com>
2025-04-06 03:16:53 +0530

    feat: add subscription schema for user channel subscriptions

---
+++ src/model/subscription.schema.js
@@ -0,0 +1,11 @@
+import mongoose,{Schema} from 'mongoose'
+
+const subscriptionSchema=new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId, //the one who is subscribing
+    ref:"User"},
+    channel:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
